e07a1b9321cc1254b100ab53c515b2a3dd5ba228
versartist <shafi258011@gmail.com>
2024-05-23 15:44:20 +0400

    menu animation

--- src/script.js
+++ src/script.js
@@ -1,15 +1,39 @@
 const menuBtn = document.querySelector('.drop-menu')
 const menuEl = document.querySelectorAll('.dmenu-el')
-const dropDown  = document.querySelector(".fullscreen-menu").style;
+const dropDown  = document.querySelector(".fullscreen-menu");
 const closeBtn = document.querySelector('.closeBtn');
+const main = document.querySelector('.content')
 
 
 menuBtn.addEventListener('click', ()=>{
-    dropDown.display = "block";
+    closeBtn.style.display = "block"
+    main.style.zIndex = -111;
+    dropDown.style.zIndex = 111;
+    dropDown.style.display = "block";
+//    dropDown.style.transform =  'translateX(100%)';
+   anime({
+    targets: dropDown,
+    opacity :['0%','100%'],
+    translateX: ['100%', '0%'],
+    duration: 150,
+    easing: 'easeOutQuad'
+});
+   
 })
 
 closeBtn.addEventListener("click",()=>{
-    dropDown.display = "none";
+    anime({
+        targets: dropDown,
+        opacity :['100%','0%'],
+        translateX: ['0%', '100%'],
+        duration: 150,
+        easing: 'easeOutQuad'
+    });
+    closeBtn.style.display = "none"
+    setTimeout(()=>{dropDown.style.display = "none";},100)
+    main.style.zIndex = 111;
+    // dropDown.style.zIndex = -111;
+    
 })
 //gsap for dmenu
 
