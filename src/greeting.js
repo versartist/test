6c4561761ee3f99aef59c53d34a8346cbc5108ed
versartist <shafi258011@gmail.com>
2024-05-28 20:23:34 +0400

    mobile screen

--- src/greeting.js
+++ src/greeting.js
@@ -1,6 +1,7 @@
 const textHolder = document.querySelector('.text');
 const stripes = document.querySelectorAll(".stripes");
 const preloader = document.querySelector('.preloader');
+const hero = document.querySelector('.hero');
 const content = document.querySelector('.content').style;
 const greetings = [
     "Hello" /* English */,
@@ -34,6 +35,7 @@ const greetings = [
 
   function displayAndAnimate() {
     content.display = "none";
+    hero.style.display = "none"
     content.zIndex = -1;
     preloader.style.zIndex = 20;
     
@@ -46,6 +48,7 @@ const greetings = [
         tl.to(textHolder,{ duration: .5 , innerHTML: `<h2 class="greeting">${greeting}</h2>`})
         .to(textHolder,{ duration: .4 , y :80 , ease : "power3.out" })
         .to(textHolder,{ duration: .4 , y : '-100vh' , ease : "power3.out" })
+        
         .to(textHolder, {duration : .1 , opacity : 0} ); 
         content.display = "block";
         tl.to(stripes,{ duration : .7 , scaleX: 0,rotateX : 5,x : 15, ease:"expo.easeInOut",stagger: 0.06 });
@@ -53,6 +56,9 @@ const greetings = [
         content.zIndex = 1;
       } else {
         tl.to(textHolder, { duration: .15, innerHTML: `<h2 class="greeting">${greeting}</h2>` }); // Update greeting text
+        if(hero.style.display == "none") {
+          hero.style.display = "flex";
+        }
       }
     });
   }
